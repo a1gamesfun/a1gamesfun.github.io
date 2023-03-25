fff59ee1a73774bf0c6715c2fa33e3c3fa786f5b
a1-creator <asbjorn15johnsen@gmail.com>
2023-03-25 14:36:00 +0100

    gsd

---
+++ js/gamespawner.js
@@ -0,0 +1,49 @@
+import fs from "fs";
+import path from "path";
+
+//const fs = require('fs');
+//const path = require('path')
+
+const jsonsInDir = fs.readdirSync('../json/games').filter(file => path.extname(file) === '.json');
+
+jsonsInDir.forEach(file => {
+    let fileData = fs.readFileSync(path.join('../json/games', file));
+    let json = JSON.parse(fileData.toString());
+    //set the string into our list of games
+    GAMES.push(json);
+});
+
+
+
+
+
+const GAMES = [ ];
+
+function loadGame() {
+
+    // get json string
+    let rnd = _.random(GAMES.length - 1);
+    let jsonstring = GAMES[rnd];
+
+    // remove one item at given index
+    GAMES.splice(rnd, 1);
+
+    // return generated json object
+    return JSON.parse(jsonstring);
+}
+
+function addItem(container, template) {
+
+    let color = COLORS[_.random(COLORS.length - 1)];
+    let num = _.random(10000);
+
+    container.append(Mustache.render(template, { color, num }));
+}
+
+$(() => {
+    const tmpl = $('#item_template').html()
+    const container = $('#app');
+
+    for (let i = 0; i < 5; i++) { addItem(container, tmpl); }
+
+});
