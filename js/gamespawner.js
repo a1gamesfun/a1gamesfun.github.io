8d6340ba2055e25aab605a5e8e229f48a8c403db
a1-creator <asbjorn15johnsen@gmail.com>
2023-03-27 09:05:10 +0200

    Update gamespawner.js

--- js/gamespawner.js
+++ js/gamespawner.js
@@ -15,19 +15,43 @@ jsonsInDir.forEach(file => {
 });
 
 */
+
+/*
 $(() => {
 
     var filepath = 'https://a1games.fun/json/games'
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", filepath, false);
 
+    var imagenames = new Array()
+
+    for (var i = 0; i < length; i++) {
+
+    }
 
 
-});
 
-
-
+});*/
 const GAMES = [ ];
+
+$(() => {
+
+
+    var people = [];
+
+    $.getJSON('json/games.json', function (data) {
+        $.each(data, () => {
+            GAMES.push(data)
+        });
+        /*
+        $.each(data, function (i, f) {
+            var tblRow = "<tr>" + "<td>" + f.firstName + "</td>" +
+                "<td>" + f.lastName + "</td>" + "<td>" + f.job + "</td>" + "<td>" + f.roll + "</td>" + "</tr>"
+            $(tblRow).appendTo("#userdata tbody");
+        });
+        */
+    });
+
 
 function loadGame() {
 
