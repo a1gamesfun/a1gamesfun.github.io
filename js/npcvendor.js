7c5e737519cff8eeeaa877484f9fa3a25e6bcf4e
a1-creator <asbjorn15johnsen@gmail.com>
2023-03-28 11:07:10 +0200

    crab island game test added

--- js/npcvendor.js
+++ js/npcvendor.js
@@ -1,5 +1,32 @@
 
 
+class House {
+    constructor(height, width, length) {
+        this.height = height;
+        this.width = width;
+        this.length = length;
+    }
+
+    measure() {
+        return `H: ${this.height}, W: ${this.width}, L: ${this.length}`
+    }
+}
+
+class Shed extends House {
+    constructor(height, width, length, tools) {
+        super(height, width, length);
+        this.tools = tools;
+    }
+}
+
+
+writeHouse = () => {
+    let h = new House("8 Meters", "12 Meters", "22 Meters");
+    console.log(h.measure());
+};
+
+writeHouse();
+
 const myObject = {
     position: (0, 0),
     Health: 100,
@@ -14,4 +41,4 @@ test = () => {
     }
 };
 
-test()
+test();
