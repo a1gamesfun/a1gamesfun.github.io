84c250ccc263c18aebfc9e44edf0fdcb1ced1a1b
a1-creator <asbjorn15johnsen@gmail.com>
2023-03-28 16:34:07 +0200

    sdf

--- js/webplayerdecoration.js
+++ js/webplayerdecoration.js
@@ -1,6 +1,8 @@
 const gamename = "crab_island"
 
 function LoadDecorations () {
-    document.getElementById("unity-webgl-logo").style.background = `url('../files/img/gameIcons/${gamename}.png') no-repeat center `
-    document.getElementById("unity-logo").style.background = `url('../files/img/gameIcons/${gamename}.png') no-repeat center `
+    document.getElementById("unity-webgl-logo").style.background = `url('../files/img/gameIcons/${gamename}.png') no-repeat center;`
+    document.getElementById("unity-logo").style.background = `url('../files/img/gameIcons/${gamename}.png') no-repeat center;`
 }
+
+LoadDecorations();
