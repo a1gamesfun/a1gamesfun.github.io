aff07d03bae646023c2238bb8ea6fe2c6ba4315e
a1-creator <asbjorn15johnsen@gmail.com>
2023-03-28 13:09:06 +0200

    fdg

---
+++ js/webplayerdecoration.js
@@ -0,0 +1,6 @@
+const gamename = "crab_island"
+
+function LoadDecorations () {
+    document.getElementById("unity-webgl-logo").style.background = `url('../files/img/loading-icons/${gamename}.png') no-repeat center `
+    document.getElementById("unity-webgl-logo").style.background = `url('../files/img/loading-icons/${gamename}.png') no-repeat center `
+}
