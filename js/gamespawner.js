9615f0f7a6be2dd1e9e50ca8a990e6687e26dab7
a1-creator <asbjorn15johnsen@gmail.com>
2023-03-29 13:33:52 +0200

    sd

--- js/gamespawner.js
+++ js/gamespawner.js
@@ -1,89 +1,80 @@
-/*
-import fs from "fs";
-import path from "path";
-
-//const fs = require('fs');
-//const path = require('path')
-
-const jsonsInDir = fs.readdirSync('../json/games').filter(file => path.extname(file) === '.json');
-
-jsonsInDir.forEach(file => {
-    let fileData = fs.readFileSync(path.join('../json/games', file));
-    let json = JSON.parse(fileData.toString());
-    //set the string into our list of games
-    GAMES.push(json);
-});
-
-*/
-
-
-fetch("https://a1games.fun/json/gamelist.json")
-    .then((response) => response.json())
-    .then((jsonresponse) =>  {
-        console.log(jsonresponse);
-
-        console.log(jsonresponse.gamelist.length)
-
-        console.log(jsonresponse.gamelist[2])
-    });
 
 
 
 
-const GAMES = [ ];
 
-/*
-$(() => {
-    console.log("Getting json file")
-    $.getJSON('../json/games/space_voyage.json', function (data) {
-        $.each(data, () => {
-            GAMES.push(data)
-            console.log("pushed data: " + data);
+// define games array
+var GAMES = []
+
+// get the array of gamenames
+// Example: [
+// "space_voyage", 
+// "crab_island", 
+// "example_name"
+// ]
+async function setGAMES()
+{
+    await fetch("https://a1games.fun/json/gamelist.json")
+        .then((response) => response.json())
+        .then((jsonresponse) =>  {
+    
+            GAMES = jsonresponse.gamelist;
+            
         });
-    });
-    
-    $.each(data, function (i, f) {
-        var tblRow = "<tr>" + "<td>" + f.firstName + "</td>" +
-            "<td>" + f.lastName + "</td>" + "<td>" + f.job + "</td>" + "<td>" + f.roll + "</td>" + "</tr>"
-        $(tblRow).appendTo("#userdata tbody");
-    });
-    
-});
-*/
-
-function loadGame() {
-
-    // get json string
-    let rnd = _.random(GAMES.length - 1);
-    let jsonstring = GAMES[rnd];
-
-    // remove one item at given index
-    GAMES.splice(rnd, 1);
-
-    // return generated json object
-    return JSON.parse(jsonstring);
 }
 
 
 
 
-function addItem(container, template) {
 
-    var gameObj = loadGame();
+
+
+async function loadGame() {
+
+    // get json string
+    let rnd = _.random(GAMES.length - 1);
+    let gamename = GAMES[rnd];
+    
+    // fetch the json game info file
+    await fetch(`https://a1games.fun/json/games/${gamename}.json`)
+        .then((response) => response.json())
+        .then((jsonresponse) =>  {
+    
+            // remove one item at given index
+            GAMES.splice(gamename, 1);
+
+            // return generated json object
+            return jsonresponse;
+            
+        });
+
+
+}
+
+
+
+
+async function addItem(container, template) {
+
+    var gameObj = await loadGame();
 
     var imagename = gameObj.imagename;
     var href = gameObj.href;
 
     container.append(Mustache.render(template, { imagename, href }));
 }
-/*
-$(() => {
+
+async function spawnHTML()
+{
+    await setGAMES();
+    // spawn the games as html
     const tmpl = $('#item_template').html();
-    const container = $('#app');
+        const container = $('#app');
+    
+        for (let i = 0; i < GAMES.length; i++)
+        {
+            addItem(container, tmpl);
+        }
+}
 
-    for (let i = 0; i < 5; i++)
-    {
-        addItem(container, tmpl);
-    }
-
-});*/
+spawnHTML();
