ce6eb368f87a0333f203a699fc699e4b54909bce
a1-creator <asbjorn15johnsen@gmail.com>
2023-04-01 22:02:03 +0200

    everything changed from static url to dynamic gameinfo json

--- js/webplayerdecoration.js
+++ js/webplayerdecoration.js
@@ -23,22 +23,7 @@ async function LoadDecorations () {
 
 
     // set game title
-    let name = gameinfo["gamename"].replace("_", " ");
-    let indexes = [];
-    for(var i = 0; i < name.length; i++) {
-        
-        if (name[i] === " ") indexes.push(i);
-    }
-
-    // always start with first chat uppercase
-    name = setCharAt(name, 0, name[0].toUpperCase());
-    // set all words after spaces uppercase
-    for (let i = 0; i < indexes.length; i++) {
-        name = setCharAt(name, indexes[i]+1, name[indexes[i]+1].toUpperCase());
-    }
-
-    // override it in html
-    document.getElementById("unity-build-title").innerHTML = name;
+    document.getElementById("unity-build-title").innerHTML = gameinfo["title"];
 
 
     // ---- DESCRIPTION: ----
