d95d7290053bf33e6faf40d04acf04a2de7dc048
a1-creator <asbjorn15johnsen@gmail.com>
2023-04-08 00:12:05 +0200

    header random fonts works alright now

--- files/js/headerdecoration.js
+++ files/js/headerdecoration.js
@@ -1,58 +1,87 @@
 
 
-const fonts = [
-    "'Roboto', sans-serif;",
-    "'Indie Flower', cursive;",
-    "'Crimson Text', serif",
-    "'Inspiration', cursive",
-    "'Righteous', cursive;",
-    "'Audiowide', cursive;",
-    "'Bangers', cursive;",
-    "'Bungee Shade', cursive;",
-    "'Creepster', cursive;",
-    "'Dancing Script', cursive;",
-    "'Delicious Handrawn', cursive;",
-    "'Major Mono Display', monospace;",
-    "'Megrim', cursive;",
-    "'Monoton', cursive;",
-    "'Mr Dafoe', cursive;",
-    "'MuseoModerno', cursive;",
-    "'Nabla', cursive;",
-    "'Pacifico', cursive;",
-    "'Press Start 2P', cursive;",
-    "'Redacted Script', cursive;",
-    "'Rubik 80s Fade', cursive;",
-    "'Rubik Moonrocks', cursive;",
-    "'Rubik Pixels', cursive;",
-    "'Rubik Vinyl', cursive;",
-    "'Ruslan Display', cursive;",
-    "'Rye', cursive;",
-    "'Sacramento', cursive;",
-    "'Sedgwick Ave Display', cursive;",
-    "'Silkscreen', cursive;",
-    "'Tilt Prism', cursive;",
-    "'Titan One', cursive;",
-    "'Tourney', cursive;",
+const fontObjects = [
+    { name: "Roboto", fallback: "sans-serif", },
+    { name: "Indie Flower", fallback: "cursive", }, 
+    { name: "Crimson Text", fallback: "cursive", }, 
+    { name: "Inspiration", fallback: "cursive", }, 
+    { name: "Righteous", fallback: "cursive", }, 
+    { name: "Righteous", fallback: "cursive", },
+    { name: "Righteous", fallback: "cursive", },
+    { name: "Righteous", fallback: "cursive", },
+    { name: "Audiowide", fallback: "cursive", },
+    { name: "Bangers", fallback: "cursive", },
+    { name: "Bungee Shade", fallback: "cursive", },
+    { name: "Bangers", fallback: "cursive", },
+    { name: "Creepster", fallback: "cursive", },
+    { name: "Dancing Script", fallback: "cursive", },
+    { name: "Delicious Handrawn", fallback: "cursive", },
+    { name: "Major Mono Display", fallback: "cursive", },
+    { name: "Megrim", fallback: "cursive", },
+    { name: "Monoton", fallback: "cursive", },
+    { name: "Mr Dafoe", fallback: "cursive", },
+    { name: "MuseoModerno", fallback: "cursive", },
+    { name: "Nabla", fallback: "cursive", },
+    { name: "Pacifico", fallback: "cursive", },
+    { name: "Press Start 2P", fallback: "cursive", },
+    { name: "Redacted Script", fallback: "cursive", },
+    { name: "Rubik 80s Fade", fallback: "cursive", },
+    { name: "Rubik Moonrocks", fallback: "cursive", },
+    { name: "Rubik Pixels", fallback: "cursive", },
+    { name: "Rubik Vinyl", fallback: "cursive", },
+    { name: "Ruslan Display", fallback: "cursive", },
+    { name: "Rye", fallback: "cursive", },
+    { name: "Sacramento", fallback: "cursive", },
+    { name: "Sedgwick Ave Display", fallback: "cursive", },
+    { name: "Silkscreen", fallback: "cursive", },
+    { name: "Tilt Prism", fallback: "cursive", },
+    { name: "Titan One", fallback: "cursive", },
+    { name: "Tourney", fallback: "cursive", },
 ];
 
-async function randomizeFont(element)
+var fontObjectsToLoad = [];
+
+function setRandomFontReferences()
 {
+    for (let i = 0; i < 2; i++) {
+        // get rnd index
+        let rnd = _.random(fontObjects.length - 1);
+        
+        // append to use later
+        fontObjectsToLoad.push(fontObjects[rnd]);
 
-    let rnd = _.random(fonts.length - 1);
-
-    element.style.fontFamily = fonts[rnd];
-    console.log(fonts[rnd])
-}
-
-async function randomizeHeaderFonts()
-{
-    let elements = [];
-    elements.push(document.getElementById("free").getElementsByClassName("header-h1")[0]);
-    elements.push(document.getElementById("browsergames").getElementsByClassName("header-h1")[0]);
-
-    for (let i = 0; i < elements.length; i++) {
-        randomizeFont(elements[i]);
+        // get the api font getter name
+        let name = fontObjects[rnd].name;
+        
+        var chars = name.split('');
+        
+        for (let i = 0; i < name.length; i++) {
+            if (name.charAt(i) == ' ')
+            {
+                chars[i] = '+';
+            }
+        }
+        name = chars.join('');
+        
+        console.log(name)
+        // create a <link/> element and append it
+        var node = document.createElement("link");
+        node.href = `https://fonts.googleapis.com/css2?family=${name}&display=swap`;
+        node.rel = `stylesheet`;
+        document.head.append(node);
     }
 }
 
-randomizeHeaderFonts();
+setRandomFontReferences();
+
+async function setFonts()
+{
+    let free = document.getElementById("free").getElementsByClassName("header-h1")[0];
+    free.style.fontFamily = `${fontObjectsToLoad[0].name}, ${fontObjectsToLoad[0].fallback}`;
+
+    let browsergames = document.getElementById("browsergames").getElementsByClassName("header-h1")[0];
+    browsergames.style.fontFamily = `${fontObjectsToLoad[1].name}, ${fontObjectsToLoad[1].fallback}`;
+}
+
+setFonts();
+
