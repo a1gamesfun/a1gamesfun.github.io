f8005b14b455334a91d6211e330653c2ec804654
a1-creator <asbjorn15johnsen@gmail.com>
2023-04-13 23:40:50 +0200

    indie ads work

---
+++ files/js/indieads.js
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+
+
+function spawnAds()
+{
+    var adContainers = document.body.getElementsByClassName("indieads");
+
+    for (let i = 0; i < adContainers.length; i++) {
+        
+        var linkElement = document.createElement("a");
+        linkElement.target = "_blank";
+        linkElement.className = "indiead-link"
+        linkElement.id = `indieads-link-${i}`;
+        
+        var imgElement = document.createElement("img");
+        imgElement.className = "indiead-img"
+        imgElement.id = `indieads-img-${i}`;
+        
+        linkElement.append(imgElement);
+        adContainers[i].append(linkElement);
+        
+        
+        fetchIndieAd(linkElement.id, imgElement.id)
+    }
+}
+
+async function fetchIndieAd(link_elementID, img_elementID) {
+    
+
+    // fetch the json game info file
+    fetch(`https://www.a1creator.com/indieads/foldernames.json`)
+        .then((response) => { return response.json(); })
+        .then((response) =>  {
+            
+            let foldernames = response["foldernames"];
+
+            let foldername = foldernames[Math.floor(Math.random() * foldernames.length)];
+
+            document.getElementById(img_elementID).src = `https://www.a1creator.com/indieads/${foldername}/${600}x${900}.png`;
+            
+            fetch(`https://www.a1creator.com/indieads/${foldername}/href`)
+            .then((response) => {
+                if (!response.ok)
+                {
+                    return "";
+                }
+                return response.text(); 
+            })
+            .then((response) => {
+                document.getElementById(link_elementID).href = `${response}`
+            })
+
+        });
+
+}
+
+spawnAds();
