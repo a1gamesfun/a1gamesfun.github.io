cc9a7f9cdc9ac00c527e61f5a9712adae3989179
a1-creator <asbjorn15johnsen@gmail.com>
2023-05-19 15:15:35 +0200

    htmlplayer

---
+++ files/js/htmlplayerloader.js
@@ -0,0 +1,25 @@
+var gameinfo = JSON.parse(localStorage.getItem("SelectedGame"));
+
+var container = document.getElementById("load-container");
+
+
+
+
+
+
+async function loadAndAdd() {
+    // fetch the json game info file
+    fetch(`https://a1games.fun/games/${gameinfo.gamename}/${gameinfo.gamename}.html`)
+        .then((response) => response.text())
+        .then((jsonresponse) =>  {
+            container.append(jsonresponse);
+        });
+
+}
+
+
+loadAndAdd();
+
+
+
+
