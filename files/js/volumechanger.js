d19bb4fc61caf1f45ee13d5ab06b2718b10b99ab
a1-creator <asbjorn15johnsen@gmail.com>
2023-05-31 15:26:57 +0200

    Update volumechanger.js

--- files/js/volumechanger.js
+++ files/js/volumechanger.js
@@ -1,10 +1,10 @@
 var slider = document.getElementById("volumeSlider");
 var output = document.getElementById("volumeNumber");
-output.innerHTML = slider.value; // Display the default slider value
+//output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
+  //output.innerHTML = this.value;
   //console.log(document.volume)
   //document.volume = this.value / 100;
   localStorage.setItem("gamevolume", this.value);
