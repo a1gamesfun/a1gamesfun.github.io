474a39575c1effec9979f403f8304d18cd3a09e3
a1-creator <asbjorn15johnsen@gmail.com>
2023-06-05 13:02:54 +0200

    cursor invasion added test

---
+++ files/js/pygameplayerloader.js
@@ -0,0 +1,25 @@
+var gameinfo = JSON.parse(localStorage.getItem("SelectedGame"));
+
+var container = document.getElementById("load-container");
+
+
+
+
+
+
+async function loadAndAdd() {
+    // fetch the json game info file
+    fetch(`https://a1games.fun/games/${gameinfo.gamename}/build/web/index.html`)
+        .then((response) => response.text())
+        .then((htmlresponse) =>  {
+            container.innerHTML = htmlresponse;
+        });
+
+}
+
+
+loadAndAdd();
+
+document.title = gameinfo.gamename;
+
+
